311b39f3ab8b26b418c7b8b59c915bf601d6ce69
Flavio Vasconcellos Correa <96501461+sheep-farm@users.noreply.github.com>
2025-10-10 10:24:43 -0300

    Fix updates

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ class MerkatoApplication(Adw.Application):
 
     def on_about_action(self, *args):
         """Callback for the app.about action."""
-        about = Adw.AboutDialog(application_name='merkato',
+        about = Adw.AboutDialog(application_name='Merkato',
                                 application_icon='com.github.sheepfarm.merkato',
                                 developer_name='Flávio de Vasconcellos Corrêa',
                                 version='0.2.0',
