acaed2ca7f2f1cf6922712ace87b8d668acea530
Flavio Vasconcellos Correa <96501461+sheep-farm@users.noreply.github.com>
2025-10-10 22:44:19 -0300

    Clean and bug fix

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.run(["pip3", "install", "pandas"])
+subprocess.run(["pip3", "install", "requests"])
+subprocess.run(["pip3", "install", "requests-futures"])
+subprocess.run(["pip3", "install", "yahooquery"])
