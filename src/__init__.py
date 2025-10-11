dae9d7ef9a45ff70dd8017af24c059e42e1e9ec8
Flavio Vasconcellos Correa <96501461+sheep-farm@users.noreply.github.com>
2025-10-11 00:34:08 -0300

    pip install in manifest

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.run(["pip3", "install", "pandas"])
-subprocess.run(["pip3", "install", "requests"])
-subprocess.run(["pip3", "install", "requests-futures"])
-subprocess.run(["pip3", "install", "yahooquery"])
