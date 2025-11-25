33736ca33e34f28f60ea06cf0a15e6f3e95575c0
Flavio Vasconcellos Correa <96501461+sheep-farm@users.noreply.github.com>
2025-11-25 19:47:06 -0300

    Update yahoo_request.py

--- src/yahoo_request.py
+++ src/yahoo_request.py
@@ -139,7 +139,9 @@ class YahooRequest(GObject.Object):
         batch_errors = []
 
         try:
-            ticker = Ticker(symbols_batch)
+            custom_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+
+            ticker = Ticker(symbols_batch, asynchronous=False)
 
             # Busca price (dados básicos)
             price_data = ticker.price
